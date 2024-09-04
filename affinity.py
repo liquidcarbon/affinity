239dd0a849bc6e92abdb97a04cf75dc95d969e89
Alexander Kislukhin <47034358+liquidcarbon@users.noreply.github.com>
2024-09-04 13:53:44 -0600

    test release wheel

--- affinity.py
+++ affinity.py
@@ -2,6 +2,8 @@ __doc__ = """
 Module for creating well-documented datasets, with types and annotations.
 """
 
+__version__ = "0.2.0"
+
 import numpy as np
 import pandas as pd
 from importlib import import_module
